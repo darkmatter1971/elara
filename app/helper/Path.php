77808b54306ba42ce7244a8948a5c8b8e7ec2966
Tafhimul kabir <tafhimulkabir@gmail.com>
2022-11-26 00:30:38 +0600

    Added path.php

---
+++ app/helper/Path.php
@@ -0,0 +1,23 @@
+<?php 
+/**
+ * A CLass to find Path.
+ *  
+ * PHP version 8.2
+ *
+ * @version v0.0.1
+ * @category Helper
+ * @license Public Domain
+ * @author Tafhimul kabir <https://github.com/tafhimulkabir>
+ **/
+declare(strict_types=1);
+
+namespace App\Helper\Path;
+
+if (!defined('Elara')) {
+    die(http_response_code(404));
+    exit;
+}
+
+class Path {
+
+}
