ff5ed2f87b29d3c4236aad2e0e336f0259acb821
Tafhimul kabir <tafhimulkabir@gmail.com>
2022-12-01 21:59:22 +0600

    Added Comment to path.php

--- app/helper/Path.php
+++ app/helper/Path.php
@@ -1,23 +1,45 @@
 <?php 
 /**
- * A CLass to find Path.
+ * A Class to find Path.
  *  
  * PHP version 8.2
  *
  * @version v0.0.1
  * @category Helper
+ * @package Elara
  * @license Public Domain
  * @author Tafhimul kabir <https://github.com/tafhimulkabir>
+ * @link  
+ * @since File available since Release of Elara v0.0.1
+ * @copyright   
  **/
+
 declare(strict_types=1);
 
 namespace App\Helper\Path;
 
+// Prevent direct access to the file.
 if (!defined('Elara')) {
     die(http_response_code(404));
-    exit;
+    return;
 }
 
-class Path {
-
+/**
+ * Short description for class
+ *
+ * Long description for class (if any)...
+ *
+ * @category Helper
+ * @package Elara
+ * @license        
+ * @since Class available since Release of Elara v0.0.1
+ */
+abstract class Path {
+    /**
+     * Properties 
+     * 
+     * 
+     * 
+     * @since Properties available since Release of Elara v0.0.1
+     */
 }
